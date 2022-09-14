dcf8b7a7a58cba00f14729d9403a938f9c48d38d
minQ <minq.an7@gmail.com>
2022-09-14 10:31:37 -0400

    Initial Commit

---
+++ todoList_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todoList_app
+//
+//  Created by 안민규 on 2022-09-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todoList_app/todoList_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoList_appApp.swift
+//  todoList_app
+//
+//  Created by 안민규 on 2022-09-14.
+//
+
+import SwiftUI
+
+@main
+struct todoList_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
